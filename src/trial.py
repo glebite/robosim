3b8a6717031fd0713a1e8862f06845eb2a8be6fc
Lazlo <glebite@gmail.com>
2020-09-27 10:30:08 -0400

    Adding in requirements.txt (yes doing this correctly for this repo and updates to the imports for trial.py

--- src/trial.py
+++ src/trial.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 import logging
 import OpenGL
-import OpenGL.GL
-import OpenGL.GLUT
-import OpenGL.GLU
+from OpenGL.GL import *
+from OpenGL.GLUT import *
+from OpenGL.GLU import *
 
 class Robosim(object):
     """
@@ -11,7 +11,17 @@ class Robosim(object):
     def __init__(self):
         pass
 
-    
+    def show_view(self):
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+        glutInit()
+        glutInitDisplayMode(GLUT_RGBA)
+        glutInitWindowSize(500,500)
+        glutInitWindowPosition(0,0)
+        self.window = glutCreateWindow("Robosim")
+        glutDisplayFunc(self.show_view)
+        glutIdleFunc(self.show_view)
+        glutMainLoop
+        
 
 def main():
     logging.basicConfig(format='%(asctime)s %(name)s'
@@ -19,6 +29,8 @@ def main():
                         filename='robosim-trial.log',
                         level=logging.DEBUG)
     logging.info("Starting main execution.")
+    R = Robosim()
+    R.show_view()
     logging.info("Completing main execution")
 
     
