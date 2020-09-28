a1eb49c1beb0867f5be8e23266bed6f0d405b259
Lazlo <glebite@gmail.com>
2020-09-28 19:29:41 -0400

    whitespace cleanup - yeah - I know - pathetic.

--- src/trial.py
+++ src/trial.py
@@ -9,19 +9,45 @@ class Robosim(object):
     """
     """
     def __init__(self):
-        pass
-
-    def show_view(self):
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+        logging.debug(f'Robosim instantiation {self}')
         glutInit()
         glutInitDisplayMode(GLUT_RGBA)
         glutInitWindowSize(500,500)
         glutInitWindowPosition(0,0)
         self.window = glutCreateWindow("Robosim")
+        if not self.window:
+            logging.debug('Failed to create self.window')
+
         glutDisplayFunc(self.show_view)
         glutIdleFunc(self.show_view)
-        glutMainLoop
-        
+        glutMainLoop()
+        logging.debug('Robosim completeed instantiation')
+
+    def iterate(self):
+        glViewport(0, 0, 500,500)
+        glMatrixMode(GL_PROJECTION)
+        glLoadIdentity()
+
+        glOrtho(0.0, 500, 0.0, 500, 0.0, 1.0)
+        glMatrixMode (GL_MODELVIEW)
+        glLoadIdentity()
+
+    def square(self):
+        # We have to declare the points in this sequence: bottom left, bottom right, top right, top left
+        glBegin(GL_QUADS) # Begin the sketch
+        glVertex2f(100, 100) # Coordinates for the bottom left point
+        glVertex2f(200, 100) # Coordinates for the bottom right point
+        glVertex2f(200, 200) # Coordinates for the top right point
+        glVertex2f(100, 200) # Coordinates for the top left point
+        glEnd()
+
+    def show_view(self):
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+        glLoadIdentity()
+        self.iterate()
+        glColor3f(3.0, 0.0, 3.0) ;# Set the color to pink
+        self.square()
+        glutSwapBuffers()
 
 def main():
     logging.basicConfig(format='%(asctime)s %(name)s'
@@ -33,5 +59,5 @@ def main():
     R.show_view()
     logging.info("Completing main execution")
 
-    
+
 main()
